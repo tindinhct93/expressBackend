8d781efbe4dc975cc9edbb38bb52d88c3a008dc3
tindinhct93 <tinhdinhct93@gmail.com>
2022-05-29 06:52:21 +0700

    Change RDS

--- config/config.js
+++ config/config.js
@@ -20,10 +20,10 @@ module.exports =
       },
   "production":
       {
-          "username":    process.env.DB_USERNAME,
-          "password":    process.env.DB_PASSWORD,
-          "database":    process.env.DB_DATABASE,
-          "host"  :      process.env.DB_HOST,
+          "username":    process.env.RDS_USERNAME,
+          "password":    process.env.RDS_PASSWORD,
+          "database":    process.env.RDS_DB_NAME,
+          "host"  :      process.env.RDS_HOSTNAME,
           "dialect":    "mysql"
       }
 }
