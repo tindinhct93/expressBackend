7d24d1f195cbfe1ba86d265a98b87584394a75c3
tindinhct93 <tinhdinhct93@gmail.com>
2022-05-29 09:12:18 +0700

    Fix AWS

--- config/config.js
+++ config/config.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 module.exports =
 
 {
-  "development" :
-  {
-    "username":    process.env.DB_USERNAME,
-    "password":    process.env.DB_PASSWORD,
-    "database":    process.env.DB_DATABASE,
-    "host"  :      process.env.DB_HOST,
-        "dialect":    "mysql"
-  },
+  // "development" :
+  // {
+  //   "username":    process.env.DB_USERNAME,
+  //   "password":    process.env.DB_PASSWORD,
+  //   "database":    process.env.DB_DATABASE,
+  //   "host"  :      process.env.DB_HOST,
+  //       "dialect":    "mysql"
+  // },
+    "development":
+        {
+            "username":    process.env.RDS_USERNAME,
+            "password":    process.env.RDS_PASSWORD,
+            "database":    process.env.RDS_DB_NAME,
+            "host"  :      process.env.RDS_HOSTNAME,
+            "dialect":    "mysql"
+        },
   "test":
       {
           "username":    process.env.DB_USERNAME,
